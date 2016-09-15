fd90b3ecc8803fef4cc049fe3e0e355a644b13c3
abby-garcia <acg361@gmail.com>
2016-09-15 17:30:54 -0400

    instructions

--- hello.php
+++ hello.php
@@ -1,5 +1,23 @@
-<?php 	
+<?php
 	echo 'Hello World';
 
 
 ?>
+
+
+<!-- How to write variables:
+$num_one = 3;
+
+Floats:
+numbers with decimals
+we get a floating point in return.
+php handles the it, so you don't have to
+
+How to dump variables to screen:
+var_dump
+
+
+
+
+
+ -->
