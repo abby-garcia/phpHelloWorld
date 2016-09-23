e8b7c24e16670045e98cc51bcbf6b6600352f472
abby-garcia <acg361@gmail.com>
2016-09-23 14:32:43 -0400

    numbers challenge

---
+++ numbers.php
@@ -0,0 +1,16 @@
+<?php
+
+//Place your code below this comment
+$integerOne = 1;
+$integerTwo =2;
+
+$integerOne += 5;
+$integerTwo -= 1;  
+
+$floatOne = 1.5;
+
+$result = $floatOne * $integerOne;
+
+echo $result;
+
+?>
